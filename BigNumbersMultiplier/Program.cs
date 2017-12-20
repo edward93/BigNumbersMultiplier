215cfb6638e318b940492f75b57f9af3901fcd22
Edward <edward.chopuryan@gmail.com>
2017-12-20 21:13:38 +0400

    Add project files.

---
+++ BigNumbersMultiplier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigNumbersMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
